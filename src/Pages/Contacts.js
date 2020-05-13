264dec6197c5372bda7f7ffa361ec575cd8d184c
Aman89Django <87711441333@mail.ru>
2020-05-13 18:35:12 +0600

    05: Add form

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
+import {Container, Form, Button} from 'react-bootstrap';
 
 class Contacts extends Component {
     render() {
         return (
-            <div>
-                Contacts
-            </div>
+            <Container style={{ width: '550px',  marginTop: '100px'}}>
+                <h1 className="text-center">Contact us</h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text>
+                            We'll never share your email with anyone else
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+            </Container>
         );
     }
 }
